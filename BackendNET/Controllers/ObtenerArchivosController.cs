4e7af7d298c30639478b4fcfa68555daaaf54620
unknown <admin@BGAI201-15.bga.upb>
2023-10-25 13:39:27 -0500

    😒

--- BackendNET/Controllers/ObtenerArchivosController.cs
+++ BackendNET/Controllers/ObtenerArchivosController.cs
@@ -1,6 +1,44 @@
-﻿namespace BackendNET.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MiReferencia;
+
+namespace BackendNET.Controllers
 {
-    public class ObtenerArchivosController
+    [ApiController]
+    [Route("file")]
+    public class ObtenerArchivosController : ControllerBase
     {
+        [HttpGet("getFiles")]
+        public async Task<IActionResult> Get([FromBody] user request)
+        {
+            if (request == null)
+            {
+                BadRequest();
+            }
+            await using (var client = new ServiceClient())
+            {
+                try
+                {
+                    loginResponse response = await client.getUserFilesAsync(int id);
+                    if (response == null || response.@return == null)
+                    {
+                        return StatusCode(StatusCodes.Status404NotFound, new { Msg = "La respuesta del SOAP es nula" });
+                    }
+                    string errorMessage = response.@return.details;
+                    if (errorMessage.Contains("Usuario no registrado"))
+                    {
+                        return StatusCode(StatusCodes.Status401Unauthorized, new { Msg = errorMessage });
+                    }
+                    return StatusCode(StatusCodes.Status200OK, new { Msg = response.@return, Code = response.@return.statusCode });
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(new { ex.Message });
+                }
+
+            }
+        }
+        
+    
     }
+   
 }
