3183bb1b25638f0f69178798ec64c8bfb4fa6f8e
Estefany Rueda Romero <yurley.rueda.2019@upb.edu.co>
2023-10-26 08:51:16 -0500

    Controlador de obtener archivos funcional

--- BackendNET/Controllers/ObtenerArchivosController.cs
+++ BackendNET/Controllers/ObtenerArchivosController.cs
@@ -1,34 +1,80 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MiReferencia;
+using Newtonsoft.Json;
+using ReferenciaActualizada;
 
 namespace BackendNET.Controllers
 {
+    public class FileR
+    {
+        public int id { get; set; }
+
+        public string name { get; set; } = null!;
+
+        public string path { get; set; } = null!;
+
+        public List<string> fileData { get; set; } = null!;
+
+        public double size { get; set; }
+
+        public int userId { get; set; }
+
+        public int folderId { get; set; }
+
+        public int nodeId { get; set; }
+    }
+
     [ApiController]
     [Route("file")]
     public class ObtenerArchivosController : ControllerBase
     {
         [HttpGet("getFiles")]
-        public async Task<IActionResult> Get([FromBody] user request)
+        public async Task<IActionResult> Get([FromBody] int id)
         {
-            if (request == null)
+
+            if (id == null)
             {
                 BadRequest();
             }
+
             await using (var client = new ServiceClient())
             {
                 try
                 {
-                    loginResponse response = await client.getUserFilesAsync(int id);
+                    getUserFilesResponse response = await client.getUserFilesAsync(id);
+
+                    String archivos = response.@return.json;
+                    Console.WriteLine(archivos);
                     if (response == null || response.@return == null)
                     {
                         return StatusCode(StatusCodes.Status404NotFound, new { Msg = "La respuesta del SOAP es nula" });
                     }
                     string errorMessage = response.@return.details;
-                    if (errorMessage.Contains("Usuario no registrado"))
+                    if (errorMessage.Contains("El usuario no tiene archivos"))
                     {
                         return StatusCode(StatusCodes.Status401Unauthorized, new { Msg = errorMessage });
                     }
-                    return StatusCode(StatusCodes.Status200OK, new { Msg = response.@return, Code = response.@return.statusCode });
+                    if (!string.IsNullOrEmpty(archivos) && archivos.Length > 2)
+                    {
+
+                        FileR resFile = JsonConvert.DeserializeObject<FileR>(archivos);
+
+                        return StatusCode(StatusCodes.Status200OK, new
+                        {
+                            statusCode = (int)response.@return.statusCode,
+                            details = "Operacion exitosa",
+                            files = resFile,
+
+                        });
+                    }
+                    else
+                    {
+                        return BadRequest(new
+                        {
+                            statusCode = (int)response.@return.statusCode,
+                            details = errorMessage
+                        });
+                    }
+
                 }
                 catch (Exception ex)
                 {
@@ -37,8 +83,8 @@ namespace BackendNET.Controllers
 
             }
         }
-        
-    
+
+
     }
-   
+
 }
